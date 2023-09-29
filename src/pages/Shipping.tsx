f55f86384a68192ad9f7455f59fc220601d107c5
mariamhus <mariamhus2000@gmail.com>
2023-09-30 02:57:51 +0300

    few edits

--- src/pages/Shipping.tsx
+++ src/pages/Shipping.tsx
@@ -1,15 +1,19 @@
 import StatusBar from "../components/StatusBar";
+import { translate } from "../i18next";
+import { useAppSelector } from "../stores/hooks";
 import { Shipment } from "../types/types";
 
 function Shipping(props:{
   OrderData:Shipment;
 }) {
+  const { language } = useAppSelector((state) => state.lang);
+
   return (
-      <div className="outline outline-gray-100 rounded-md w-full p-5   ">
-        <div className="flex flex-row justify-between  flex-wrap sm:p-0 ">
+      <div className="outline outline-gray-100 rounded-md w-full p-5">
+        <div className="md:flex md:flex-row md:justify-between md:text-start flex-wrap sm:p-0 sm:block sm:text-center ">
           <div className="flex flex-col">
             <div className="font-thin text-gray-400 whitespace-wrap">
-              Tracking Number {props?.OrderData?.TrackingNumber}
+              {translate("Tracking Number",language)} {props?.OrderData?.TrackingNumber}
             </div>
             <div className="font-bold text-red-600 whitespace-wrap break-words">
               {props?.OrderData?.CurrentStatus?.state}
@@ -17,18 +21,18 @@ function Shipping(props:{
           </div>
           <div className="flex flex-col">
             <p className="font-thin text-gray-400">
-              Latest Update
+            {translate("Latest Update",language)}
             </p>
             <div className="font-bold whitespace-wrap">
               {new Date(props?.OrderData?.CurrentStatus?.timestamp).toLocaleString()}
             </div>
           </div>
           <div className="flex flex-col">
-            <p className="font-thin text-gray-400">Provider</p>
+            <p className="font-thin text-gray-400">{translate("Provider",language)}</p>
             <div className="font-bold whitespace-wrap">{props?.OrderData?.provider}</div>
           </div>
           <div className="flex flex-col whitespace-wrap">
-            <p className="font-thin text-gray-400">Promised Date</p>
+            <p className="font-thin text-gray-400">{translate("Promised Date",language)}</p>
             <div className="font-bold whitespace-wrap">{new Date(props?.OrderData?.PromisedDate).toLocaleString()}</div>
           </div>
         </div>
